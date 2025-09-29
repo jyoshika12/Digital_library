0188481c9b8f90ab10a6ba5debc0ebb95a619321
jyoshika12 <j.jyoshika12@gmail.com>
2025-09-29 21:02:08 +0530

    Update book.js

--- backend/models/book.js
+++ backend/models/book.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 // Define book schema with specified fields
 const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },            // Book title
-  author: { type: String, required: true },           // Book author
-  genre: { type: String, required: true },            // Book genre
-  publishedYear: { type: Number, required: true },    // Year of publication
-  pages: { type: Number, required: true },            // Number of pages
-  coverImageUrl: { type: String, required: false },   // URL for the cover image
-  contentUrl: { type: String, required: true },       // URL to PDF/Word document
-  category: { type: String, required: true },         // Book category (e.g., Computer Science, Mathematics, etc.)
+  title: { type: String, required: true },            
+  author: { type: String, required: true },           
+  genre: { type: String, required: true },            
+  publishedYear: { type: Number, required: true },   
+  pages: { type: Number, required: true },            
+  coverImageUrl: { type: String, required: false },   
+  contentUrl: { type: String, required: true },       
+  category: { type: String, required: true },        
 });
 
 // Create book model
