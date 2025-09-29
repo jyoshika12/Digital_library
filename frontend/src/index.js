a9e248df82dcb932310e0a402ef36154aa204e78
jyoshika12 <j.jyoshika12@gmail.com>
2025-09-29 20:54:52 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import './index.css'; // Optional: include global styles
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 
 import React from 'react';
